d2a9c926ad52ce10fcb082482399d0f392e6c678
caizicheng <netfutureking@gmail.com>
2018-04-01 13:53:00 -0500

    Update

--- Section 3/Exercise3.1.R
+++
@@ -1,3 +0,0 @@
-setwd("C:\\Users\\zc3763\\Dropbox\\Courses\\18SP\\SDS383D\\git\\SDS383D\\Section 3")
-rawData = read.csv("pima.csv")
-rawData$intercept = rep(1,nrow(rawData))
--- Section3/MATLAB/3.9/run_poisson_stan.R
+++ Section3/MATLAB/3.9/run_poisson_stan.R
@@ -16,5 +16,5 @@ resStan<-stan(model_code=stan_code,data=tea,chains=3,iter=3000,warmup=1000,thin=
 traceplot(resStan, pars = c("beta"), inc_warmup = FALSE) #set inc_warmup = TRUE to see burn in
 beta1 = mean(resStan@sim[["samples"]][[1]][["beta[1]"]]+resStan@sim[["samples"]][[2]][["beta[1]"]]+resStan@sim[["samples"]][[3]][["beta[1]"]])/3
 beta2 = mean(resStan@sim[["samples"]][[1]][["beta[2]"]]+resStan@sim[["samples"]][[2]][["beta[2]"]]+resStan@sim[["samples"]][[3]][["beta[2]"]])/3
-yhat = beta2*uncensored_data$GRADE +beta1
+yhat = exp(beta2*uncensored_data$GRADE +beta1)
 rmse(uncensored_data$ACTIONS,yhat)
